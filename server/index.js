00ae15a316989b5963d65b055b35616a94af2465
olakuma <paul_kumapayi@yahoo.com>
2023-10-26 14:50:53 -0400

    starting API

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 // express.js
 const express = require('express');
-const app = express();
+const productController = require('./controllers/product')
+const app = express();  //instantiate express
 
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World\n');
-});
+    res.send('Hello World!');
+})
+.use('/products', productController)
 
 console.log('1: Trying to start server')
 
