e4c761ce731be43bbf0af2f774a8b6339b2e9fe6
olakuma <paul_kumapayi@yahoo.com>
2023-11-30 14:14:08 -0500

    generate jwt

--- server/models/users.js
+++ server/models/users.js
@@ -68,6 +68,10 @@
 * @type { {users: User[]} }
 */
 const data = require("../data/users.json");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = process.env.JWT_SECRET
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN
 
 /**
 * @returns {User[]} An array of products.
@@ -88,14 +92,14 @@ function get(id) {
 }
 
 function search(query) {
- return data.users.filter(x => {
-   return (
-     x.firstName.toLowerCase().includes(query.toLowerCase()) ||
-     x.lastName.toLowerCase().includes(query.toLowerCase()) ||
-     x.email.toLowerCase().includes(query.toLowerCase()) ||
-     x.username.toLowerCase().includes(query.toLowerCase()) 
-   );
- });
+  return data.users.filter(x => {
+    return (
+      x.firstName.toLowerCase().includes(query.toLowerCase()) ||
+      x.lastName.toLowerCase().includes(query.toLowerCase()) ||
+      x.email.toLowerCase().includes(query.toLowerCase()) ||
+      x.username.toLowerCase().includes(query.toLowerCase()) 
+    );
+  });
 }
 
 /**
@@ -103,13 +107,13 @@ function search(query) {
 * @returns {User} The created user.
 */
 function create(values) {
- const newItem = {
-   id: data.users.length + 1,
-   ...values,
- };
+  const newItem = {
+    id: data.users.length + 1,
+    ...values,
+  };
 
- data.users.push(newItem);
- return newItem;
+  data.users.push(newItem);
+  return newItem;
 }
 
 /**
@@ -117,47 +121,49 @@ function create(values) {
 * @returns {User} The created user.
 */
 function register(values) {
- // register is like create but with validation
- // and some extra logic
+  // register is like create but with validation
+  // and some extra logic
 
- const exists = data.users.some(x => x.username === values.username);
- if(exists) {
-   throw new Error('Username already exists');
- }
+  const exists = data.users.some(x => x.username === values.username);
+  if(exists) {
+    throw new Error('Username already exists');
+  }
 
- if(values.password.length < 8) {
-   throw new Error('Password must be at least 8 characters');
- }
+  if(values.password.length < 8) {
+    throw new Error('Password must be at least 8 characters');
+  }
 
- // TODO: Make sure user is create with least privileges
+  // TODO: Make sure user is create with least privileges
 
- const newItem = {
-   id: data.users.length + 1,
-   ...values,
- };
+  const newItem = {
+    id: data.users.length + 1,
+    ...values,
+  };
 
- data.users.push(newItem);
- return newItem;
+  data.users.push(newItem);
+  return newItem;
 
 }
 
 /**
 * @param {string} email
 * @param {string} password
-* @returns {User} The created user.
+* @returns { Promise<{ user: User, token: string }>} The created user.
 */
-function login(email, password) {
+async function login(email, password) {
 
- const item = data.users.find(x => x.email === email);
- if(!item) {
-   throw new Error('User not found');
- }
+  const item = data.users.find(x => x.email === email);
+  if(!item) {
+    throw new Error('User not found');
+  }
 
- if(item.password !== password) {
-   throw new Error('Wrong password');
- }
-
- return item;
+  if(item.password !== password) {
+    throw new Error('Wrong password');
+  }
+  
+  const user = { ...item, password: undefined, }
+  const token = await generateJWT(user);
+  return { user, token };
 }
 
 /**
@@ -165,29 +171,52 @@ function login(email, password) {
 * @returns {User} The updated user.
 */
 function update(newValues) {
- const index = data.users.findIndex(p => p.id === newValues.id);
- if(index === -1) {
-   throw new Error('User not found');
- }
- data.users[index] = {
-   ...data.users[index],
-   ...newValues,
- };
- return data.users[index];
+  const index = data.users.findIndex(p => p.id === newValues.id);
+  if(index === -1) {
+    throw new Error('User not found');
+  }
+  data.users[index] = {
+    ...data.users[index],
+    ...newValues,
+  };
+  return data.users[index];
 }
 
 /**
 * @param {number} id - The user's ID.
 */
 function remove(id) {
- const index = data.users.findIndex(x => x.id === id);
- if(index === -1) {
-   throw new Error('User not found');
- }
- data.users.splice(index, 1);
+  const index = data.users.findIndex(x => x.id === id);
+  if(index === -1) {
+    throw new Error('User not found');
+  }
+  data.users.splice(index, 1);
 }
 
+function generateJWT(user) {
+  return new Promise((resolve, reject) => {
+    jwt.sign(user, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN }, (err, token) => {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(token);
+      }
+    });
+  })
+}
+
+function verifyJWT(token) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(user);
+      }
+    });
+  })
+}
 
 module.exports = {
- getAll, get, search, create, update, remove, login, register
+ getAll, get, search, create, update, remove, login, register, generateJWT, verifyJWT
 };
